9b64292c408f1f59448c5b268c9aedfa1c7726d0
Auric Vente <mprops@protonmail.com>
2023-12-22 19:12:51 -0600

    Mods

--- create.rb
+++ create.rb
@@ -7,9 +7,10 @@ end
 name = ARGV[0]
 base = "/home/yo/foxprofiles/base"
 path = "/home/yo/foxprofiles/#{name}"
+firefox = "firefox-developer-edition"
 
-`firefox-developer-edition -CreateProfile "#{name} #{path}"`
+`#{firefox} -CreateProfile "#{name} #{path}"`
 `cp -R #{base}/chrome #{path}/.`
 `cp -R #{base}/extensions #{path}/.`
 `cp #{base}/user.js #{path}/user.js`
-`firefox-developer-edition -P "#{name}"`
+`#{firefox} -P "#{name}"`
