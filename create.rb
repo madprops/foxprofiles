c615ce49a411fcad38be491c8658b988b4d24586
Auric Vente <mprops@protonmail.com>
2023-12-22 22:00:10 -0600

    Mods

--- create.rb
+++ create.rb
@@ -8,22 +8,30 @@ end
 
 name = ARGV[0]
 firefox = "firefox-developer-edition"
-base = File.dirname(File.expand_path(__FILE__))
 profiles = File.join(Dir.home, "foxprofiles")
-path = File.join(profiles, name)
+$base = File.dirname(File.expand_path(__FILE__))
+$path = File.join(profiles, name)
 
 unless File.directory?(profiles)
 	FileUtils.mkdir_p(profiles)
 	puts "Created: #{profiles}"
 end
 
+def dirbase(d)
+	File.join($base, d)
+end
+
+def dirpath(d)
+	File.join($path, d)
+end
+
 puts "Creating profile..."
-`#{firefox} -CreateProfile "#{name} #{path}"`
+`#{firefox} -CreateProfile "#{name} #{$path}"`
 
 puts "Copying files..."
-FileUtils.cp_r(File.join(base, "chrome"), File.join(path, "chrome"))
-FileUtils.cp_r(File.join(base, "extensions"), File.join(path, "extensions"))
-FileUtils.cp(File.join(base, "user.js"), File.join(path, "user.js"))
+FileUtils.cp_r(dirbase("chrome"), dirpath("chrome"))
+FileUtils.cp_r(dirbase("extensions"), dirpath("extensions"))
+FileUtils.cp(dirbase("user.js"), dirpath("user.js"))
 
 puts "Launching Firefox..."
 `#{firefox} -P "#{name}"`
