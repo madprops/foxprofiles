d31915713dd1a32f8e318d6b5541ce734b74c8bc
Auric Vente <mprops@protonmail.com>
2023-12-22 21:46:32 -0600

    Mods

--- create.rb
+++ create.rb
@@ -1,16 +1,30 @@
 #!/usr/bin/env ruby
+require "fileutils"
+
 if ARGV.empty?
 	puts "Usage: create.rb <profile_name>"
 	exit
 end
 
 name = ARGV[0]
-base = "/home/yo/foxprofiles/base"
-path = "/home/yo/foxprofiles/#{name}"
+base = File.dirname(File.expand_path(__FILE__))
+profiles = File.join(Dir.home, "foxprofiles")
 firefox = "firefox-developer-edition"
 
+unless File.directory?(profiles)
+	FileUtils.mkdir_p(profiles)
+	puts "Created: #{profiles}"
+end
+
+path = File.join(profiles, name)
+
+puts "Creating profile..."
 `#{firefox} -CreateProfile "#{name} #{path}"`
+
+puts "Copying files..."
 `cp -R #{base}/chrome #{path}/.`
 `cp -R #{base}/extensions #{path}/.`
 `cp #{base}/user.js #{path}/user.js`
+
+puts "Launching Firefox..."
 `#{firefox} -P "#{name}"`
